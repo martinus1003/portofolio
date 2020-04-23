0ef136e118dbfebece9f35ac185d909f39aa2799
martinus1003 <martinusbudiman98@gmail.com>
2020-04-23 17:52:21 +0700

    feature(INT-1): change position content
    -change education.js
    -change experience.js

--- src/components/education.js
+++ src/components/education.js
@@ -6,13 +6,13 @@ class Education extends Component {
     return (
       <div>
         <Grid>
-          <Cell col={4}>
+          <Cell col={6}>
+            <h4 style={{ marginTop: "0px" }}>{this.props.schoolName}</h4>
+          </Cell>
+          <Cell col={6}>
             <p>
               {this.props.startYear} - {this.props.endYear}
             </p>
-          </Cell>
-          <Cell col={8}>
-            <h4 style={{ marginTop: "0px" }}>{this.props.schoolName}</h4>
             <p>{this.props.schoolDescription}</p>
           </Cell>
         </Grid>
--- src/components/experience.js
+++ src/components/experience.js
@@ -6,13 +6,13 @@ class Experience extends Component {
     return (
       <div>
         <Grid>
-          <Cell col={4}>
+          <Cell col={6}>
+            <h4 style={{ marginTop: "0px" }}>{this.props.jobName}</h4>
+          </Cell>
+          <Cell col={6}>
             <p>
               {this.props.startYear} - {this.props.endYear}
             </p>
-          </Cell>
-          <Cell col={8}>
-            <h4 style={{ marginTop: "0px" }}>{this.props.jobName}</h4>
             <p>{this.props.jobDescription}</p>
           </Cell>
         </Grid>
